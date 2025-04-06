22c1a8754e7b0db9288f122cf51799165a93545b
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2025-04-06 05:31:51 -0300

    feat: add new endpoint

--- zicoagents_dockers/frontend/config.ts
+++ zicoagents_dockers/frontend/config.ts
@@ -2,9 +2,8 @@ export const routerAddress = "0x111111125421cA6dc452d289314280a0f8842A65";
 export const oneInchNativeToken = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 
 const getBackendUrl = () => {
-  if (typeof window === 'undefined') return 'http://localhost:8080';
-  const host = window.location.hostname;
-  return `http://${host}:8080`;
+  const host = process.env.NEXT_PUBLIC_API_URL;
+  return host || 'http://localhost:8080';
 };
 
 export const availableAgents: {

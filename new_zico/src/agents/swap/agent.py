6a74429ecbb5b7d54d511c0e380aa55703983b82
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2026-02-17 20:19:29 -0300

    emprovee swap flow

--- new_zico/src/agents/swap/agent.py
+++ new_zico/src/agents/swap/agent.py
@@ -1,5 +1,6 @@
 import logging
 from src.agents.swap.tools import get_tools
+from src.agents.portfolio.tools import get_user_portfolio_tool
 from langgraph.prebuilt import create_react_agent
 
 logger = logging.getLogger(__name__)
@@ -11,6 +12,6 @@ class SwapAgent:
         self.llm = llm
         self.agent = create_react_agent(
             model=llm,
-            tools=get_tools(),
+            tools=get_tools() + [get_user_portfolio_tool],
             name="swap_agent"
         )
